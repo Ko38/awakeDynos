6c12b722abdbac44b773c284e26054e91e191815
thenewstockton <phillip5914619@gmail.com>
2019-11-11 12:24:50 -0800

    awake dynos

---
+++ app.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+const port = process.env.PORT || 5000;
+setInterval(function() {
+    http.get("http://stockoverflowku.herokuapp.com/");
+}, 300000); // every 5 minutes (300000)
+
+http.createServer(function (req, res) { 
+    console.log("Request: " + req.method + " to " + req.url); 
+    res.writeHead(200, "OK"); 
+    res.write("<h1>Hello</h1>Node.js is working"); 
+    res.end(); 
+   }).listen(port); 
+console.log("Ready on port 5000");
+   
